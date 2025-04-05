f8820c8d0ea4bbb0ad565eda761b3fdbf1e236d1
Aizaz Sh <aizazshahid786@gmail.com>
2025-04-05 11:18:32 +0500

    will modify the default seeder file for User adding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,9 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -15,9 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+		User::create([
+			'name' => 'Admin',
+			'email' => 'admin@example.com',
+			'password' => Hash::make('let123456')
+		]);
+
+
     }
 }
